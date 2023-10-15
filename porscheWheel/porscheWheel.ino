b6f11f017bad5e2ceeade680c9353c2da12d63eb
Clumpsypenguin <geerinckxseppe@gmail.com>
2023-10-15 23:51:51 +0200

    enable joystick mode

--- porscheWheel/porscheWheel.ino
+++ porscheWheel/porscheWheel.ino
@@ -1,5 +1,6 @@
 #include <Keypad.h>
 #include <RotaryEncoder.h>
+#include <Joystick.h>
 
 // Constants
 #define ENABLE_PULLUPS
@@ -26,44 +27,51 @@ Keypad buttbx = Keypad(makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS);
 RotaryEncoder encoder1(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
 RotaryEncoder encoder2(PIN_IN3, PIN_IN4, RotaryEncoder::LatchMode::TWO03); // Second encoder
 
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
+  JOYSTICK_TYPE_JOYSTICK, 32, 0, // Button Count, Hat Switch Count
+  false, false, false, false, false, false, // X, Y, Z, Rx, Ry, and Rz Axis
+  false, false, false,                     // rudder, throttle, and accelerator 
+  false, false, false);                    // brake, steering, and virtual joystick
+
 void setup() {
   Serial.begin(9600);
   rotary_init();
+  
+  // Set the Joystick to be a game controller.
+  Joystick.begin();
 }
 
 void loop() { 
   checkAllButtons();
-  checkEncoder(encoder1); // Encoder 1
-  checkEncoder(encoder2); // Encoder 2
+  checkEncoder(encoder1, 30, 31); // Encoder 1
+  checkEncoder(encoder2, 32, 33); // Encoder 2
 }
 
 void checkAllButtons() {
   if (buttbx.getKeys()) {
     for (int i=0; i<LIST_MAX; i++) {
       if (buttbx.key[i].stateChanged) {
-        switch (buttbx.key[i].kstate) {  
-          case PRESSED:
-          case HOLD:
-            Serial.println("Button pressed");
-            break;
-          case RELEASED:
-          case IDLE:
-            break;
-        }
+        bool isPressed = (buttbx.key[i].kstate == PRESSED) || (buttbx.key[i].kstate == HOLD);
+        Joystick.setButton(buttbx.key[i].kchar, isPressed); // Set the button state in Joystick
       }   
     }
   }
 }
 
-void checkEncoder(RotaryEncoder& encoder) {
+void checkEncoder(RotaryEncoder& encoder, int buttonCW, int buttonCCW) {
   static int pos = 0;
   encoder.tick();
   int newPos = encoder.getPosition();
   if (pos != newPos) {
-    Serial.print("Encoder position: ");
-    Serial.print(newPos);
-    Serial.print(" Direction: ");
-    Serial.println((int)(encoder.getDirection()));
+    if (newPos > pos) {
+      Joystick.setButton(buttonCW, 1);
+      delay(50); // Small delay to ensure the button press is detected by the game
+      Joystick.setButton(buttonCW, 0);
+    } else {
+      Joystick.setButton(buttonCCW, 1);
+      delay(50); // Small delay to ensure the button press is detected by the game
+      Joystick.setButton(buttonCCW, 0);
+    }
     pos = newPos;
   }
 }
